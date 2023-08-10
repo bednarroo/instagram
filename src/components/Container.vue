1ff904295bd0145ef899dad80eda1ceebafd615f
Sebastian <sebastianwuczkowski@gmail.com>
2023-08-10 12:11:39 +0200

    create Navigation

---
+++ src/components/Container.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+.container{
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    background-color: #001529;
+    margin: 0 auto;
+}
+</style>
