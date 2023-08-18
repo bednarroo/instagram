08afd5a3f6a40f8438a2a2435c495dea4610d33f
Sebastian <sebastianwuczkowski@gmail.com>
2023-08-18 16:59:23 +0200

    create table with posts and beautify input

--- src/components/UploadPhotoModal.vue
+++ src/components/UploadPhotoModal.vue
@@ -2,33 +2,61 @@
     <div>
       <AButton type="primary" @click="showModal">Upload photo</AButton>
       <AModal v-model:open="open" title="Upload photo" @ok="handleOk">
-        <input type="file" accept=".jpeg, .png" @change="handleUploadChange">
-        <AInput
-            placeholder="Caption..."
-            :v-model="caption"
-            :maxlength="50"
-        >
-        </AInput>
+        <div v-if="!loading">
+            <input type="file" accept=".jpeg, .png" @change="handleUploadChange">
+            <AInput
+                placeholder="Caption..."
+                :v-model="caption"
+                :maxlength="50"
+                >
+            </AInput>
+            <ATypographyText v-if="errorMessage" type="danger" > {{ errorMessage }} </ATypographyText>
+        </div>
+        <div class="spinner" v-else>
+            <ASpin></ASpin>
+        </div>
       </AModal>
     </div>
   </template>
   <script setup>
   import { ref } from 'vue';
   import {supabase} from '../supabase'
-  const open = ref(false);
-  const caption = ref("");
+  import {useUserStore} from '../stores/users'
+  import { storeToRefs } from 'pinia';
+
+  const userStore = useUserStore()
+
+  const {user} = storeToRefs(userStore)
+
+  const loading = ref(false)
+  const open = ref(false)
+  const caption = ref("")
   const file = ref(null)
+  const errorMessage = ref("")
   
   const showModal = () => {
     open.value = true;
   };
   
   const handleOk = async () => {
+    loading.value = true
     const fileName = Math.floor(Math.random()*1000000000000000000)
     if(file.value){
-        const response = await supabase.storage.from('images').upload('public/' + fileName, file.value)
-        console.log(response)
+        const {data, error} = await supabase.storage.from('images').upload('public/' + fileName, file.value)
+        if(error){
+            loading.value = false
+            return errorMessage.value("Unable to uplad image")
+        }
+
+        await supabase.from('posts').insert({
+            url: data.path,
+            caption: caption.value,
+            owner_id: user.value.id
+        })
+
     }
+
+    loading.value = false
   };
 
   const handleUploadChange = (e) => {
@@ -40,7 +68,13 @@
 
 <style scoped>
 
-input{
-    margin-top: 10px
-}
+    input{
+       margin-top: 10px
+    }
+
+    .spinner {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
 </style>
