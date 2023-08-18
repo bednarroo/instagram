a8d08e3f45912d22dbb4b205f17b1646ecbfa5d1
Sebastian <sebastianwuczkowski@gmail.com>
2023-08-18 15:28:10 +0200

    uplad photo

--- src/components/UploadPhotoModal.vue
+++ src/components/UploadPhotoModal.vue
@@ -2,14 +2,21 @@
     <div>
       <AButton type="primary" @click="showModal">Upload photo</AButton>
       <AModal v-model:open="open" title="Upload photo" @ok="handleOk">
-        <input type="file" accept=".jpeg, .png">
-        <AInput placeholder="Caption..."></AInput>
+        <input type="file" accept=".jpeg, .png" @change="handleUploadChange">
+        <AInput
+            placeholder="Caption..."
+            :v-model="caption"
+            :maxlength="50"
+        >
+        </AInput>
       </AModal>
     </div>
   </template>
   <script setup>
   import { ref } from 'vue';
   const open = ref(false);
+  const caption = ref("");
+  const file = ref()
   
   const showModal = () => {
     open.value = true;
@@ -19,6 +26,12 @@
     console.log(e);
     open.value = false;
   };
+
+  const handleUploadChange = (e) => {
+    if(e.target.files[0]){
+        file.value = e.target.files[0]
+    }
+  }
   </script>
 
 <style scoped>
