1ee3229fadfaa25a97a28820ff4db5d93b56b39c
Sebastian <sebastianwuczkowski@gmail.com>
2023-09-01 15:31:12 +0200

    add global variable to DB

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,9 @@
 import {createClient} from "@supabase/supabase-js"
 
-const supabaseUrl = 'https://vfxbeefgfhemwvzdsabt.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZmeGJlZWZnZmhlbXd2emRzYWJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE3NTgxODgsImV4cCI6MjAwNzMzNDE4OH0.fT63IY0CRc7gihKTROHPWBHwe8nu9vm2lVPscUZVAMU'
+const {VITE_SUPABASE_URL, VITE_SUPABASE_KEY} = import.meta.env
+
+const supabaseUrl = VITE_SUPABASE_URL
+const supabaseKey = VITE_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
 export default supabase;
